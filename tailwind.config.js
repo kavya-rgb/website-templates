692ffbb33dcd332756a688c647e27ae613c5a4f8
kavya-rgb <68847379+kavya-rgb@users.noreply.github.com>
2022-02-27 17:44:36 +0530

    Tailwind Aspect Ratio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,30 @@
 module.exports = {
   content: ["index.html","/node_modules/flowbite/dist/flowbite.js"],
   theme: {
-    extend: {},
+    aspectRatio: {
+      auto: 'auto',
+      square: '1 / 1',
+      video: '16 / 9',
+      1: '1',
+      2: '2',
+      3: '3',
+      4: '4',
+      5: '5',
+      6: '6',
+      7: '7',
+      8: '8',
+      9: '9',
+      10: '10',
+      11: '11',
+      12: '12',
+      13: '13',
+      14: '14',
+      15: '15',
+      16: '16',
+    }
+  },
+  variants: {
+    aspectRatio: ['responsive', 'hover']
   },
   plugins: [require('flowbite/plugin','@tailwindcss/aspect-ratio')],
 }
